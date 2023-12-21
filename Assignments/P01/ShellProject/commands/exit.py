8e5bd204e590cf90bbe09e60d95b5d1ce39643ea
khethulwazi kunene <43411903+khethulwazi@users.noreply.github.com>
2023-12-21 13:44:29 -0600

    Create exit.py

---
+++ Assignments/P01/ShellProject/commands/exit.py
@@ -0,0 +1,7 @@
+#Ends the shell program with a system call
+import sys
+
+def exit():
+    #A successful exit(0) leaves googdbye message for user
+    #end and close the shell program
+    sys.exit("\nGoodbye!")
